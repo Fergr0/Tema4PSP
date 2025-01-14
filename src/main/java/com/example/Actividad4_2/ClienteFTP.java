bd965551d6b7314cf1c9b30d788cdfd2647043e3
Fergr0 <fergr01bbx@gmail.com>
2025-01-14 14:24:38 +0100

    primera conexion al servidor FTP como ClienteFTP

--- src/main/java/com/example/Actividad4_2/ClienteFTP.java
+++ src/main/java/com/example/Actividad4_2/ClienteFTP.java
@@ -1,7 +1,28 @@
 package com.example.Actividad4_2;
 
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+import java.io.IOException;
+
 public class ClienteFTP {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         FTPClient cliente = new FTPClient();
+        String servFTP = "ftpupload.net";
+        System.out.println("Nos conectamos a: " + servFTP);
+        cliente.connect(servFTP);
+        System.out.println(cliente.getReplyString());
+
+        int respuesta = cliente.getReplyCode();
+        System.out.println("Respuesta: " + respuesta);
+
+        if(!FTPReply.isPositiveCompletion(respuesta)) {
+            cliente.disconnect();
+            System.out.println("Conexión rechazada" + respuesta);
+            System.exit(0);
+        }
+
+        cliente.disconnect();
+        System.out.println("Conexión finalizada");
     }
 }
